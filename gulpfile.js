7c9dba1aeae866ea63f106a8c829c3f9c95a64b7
huangjunjie <946008612@qq.com>
2018-04-28 14:10:54 +0800

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ let cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var revCollector = require('gulp-rev-collector'); 
 var rev = require('gulp-rev');
+var rename = require("gulp-rename");
 var clean = require('gulp-clean');
 var minifyHtml = require('gulp-minify-html-2');
+var RevAll = require('gulp-rev-all');
 var less = require('gulp-less');
 var plumber = require('gulp-plumber');  //处理管道崩溃问题
 var notify = require('gulp-notify');  //报错与不中断当前任务
@@ -36,6 +38,7 @@ gulp.task('start', function () {
   gulp.watch(['**/*.html', '**/css/*.css', '**/js/*.js'], {cwd: 'src'}, reload);
 });
 
+// 监听less
 gulp.task('start_less', ['lessWatch', 'start']);
 
 /**
@@ -55,59 +58,79 @@ gulp.task('lessWatch', function () {
 /**
  * 压缩打包
 */
-gulp.task('build', ['minify_js', 'minify_css', 'images'], function () {
-  gulp.start('rev_jsp');
+
+var temp_path = './temp';
+var build_path = './build';
+
+gulp.task('build', ['less', 'minify_js', 'minify_css', 'images'], function () {
+  gulp.start('rev_html');
+  gulp.start('rev_all');
 });
+
+gulp.task('build_jsp', ['less', 'minify_js', 'minify_css', 'images'], function () {
+  gulp.start('rev_html');
+  gulp.start('rev_jsp');
+  gulp.start('rev_all');
+})
 
 gulp.task('minify_js', ['clean'], function () {
   /**
    * 压缩js
    */
-  return gulp.src('./src/static/js/*.js')
+  return gulp.src('./src/*/js/*.js')
     .pipe(uglify())
-    .pipe(rev())
-    .pipe(gulp.dest('./build/static/js'))
-    .pipe(rev.manifest({
-      path: 'rev-js-manifest.json'
-    }))
-    .pipe(gulp.dest('./rev'))
+    .pipe(gulp.dest(temp_path))
 });
 
 gulp.task('minify_css', ['clean'], function () {
   /**
    * 压缩css
    */
-  return gulp.src('./src/static/css/*.css')
-    .pipe(cleanCSS())
-    .pipe(rev())
-    .pipe(gulp.dest('./build/static/css'))
-    .pipe(rev.manifest({
-      path: 'rev-css-manifest.json'
-    }))
-    .pipe(gulp.dest('./rev'))
+  return gulp.src('./src/**/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest(temp_path))
 });
 
 gulp.task('images', ['clean'], function () {
   /**
    * 图片搬迁
    */
-  return gulp.src('./src/static/images/*')
-    .pipe(gulp.dest('./build/static/images'))
+  return gulp.src('./src/**/images/*')
+    .pipe(gulp.dest(temp_path))
 });
 
 gulp.task('clean', function () {
-  return gulp.src(['build', 'rev']).pipe(clean());
+  return gulp.src(['build', 'temp']).pipe(clean());
+});
+
+gulp.task('rev_all', ['rev_html'], function () {
+  return gulp.src(temp_path + '/**')
+    .pipe(RevAll.revision({
+      dontRenameFile: ['.html', '.jsp', 'min.js', 'min.css']
+    }))
+    .pipe(gulp.dest(build_path));
 });
 
 /**
  * 替换版本号，压缩html（script压缩暂时没有处理）
  */
-gulp.task('rev_jsp', function () {
-  return gulp.src(['./rev/*.json', './src/jsp/*.jsp'])
-    .pipe(revCollector())
+gulp.task('rev_html', function () {
+  return gulp.src(['./src/**/*.html'])
     .pipe(minifyHtml({
       empty: true,  
       spare: true
     }))
-    .pipe(gulp.dest('./build/jsp'));
+    .pipe(gulp.dest(temp_path));
+});
+
+/**
+ * 替换版本号，压缩html（script压缩暂时没有处理）jsp
+ */
+gulp.task('rev_jsp', function () {
+    return gulp.src(['./src/**/*.jsp'])
+    .pipe(minifyHtml({
+      empty: true,  
+      spare: true
+    }))
+    .pipe(gulp.dest(temp_path));
 });
